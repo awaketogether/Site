f6792ae310b12d5d157c33751950a712244e3b23
Michele <bluedraggy@gmail.com>
2020-06-01 23:44:27 +0200

    chore(master) update base installs

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Awake Site`,
     description: `Let's correctly detect sleep disorders.`,
-    author: `@leodau`,
+    author: `@michele_leo`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,9 +24,25 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/awake-icon.png`,
       },
     },
-    `gatsby-plugin-styled-components`
+    `gatsby-plugin-styled-components`,
+    {
+        resolve: `gatsby-plugin-google-fonts`,
+        options: {
+          fonts: [
+            `ubuntu mono`,
+          ],
+          display: 'swap'
+        }
+    },
+    {
+        resolve: `gatsby-plugin-mailchimp`,
+        options: {
+            endpoint: "https://gmail.us18.list-manage.com/subscribe/post?u=6bf15e3b21a88c01bf8b05379&amp;id=d6ba68f9fe",
+            timeout: 3500
+        }
+    }
   ],
 }
